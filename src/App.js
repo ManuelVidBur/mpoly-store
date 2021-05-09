52647c5d0cc1d5ca30b86e807cef6fdb6ecbd907
Manuel <manuel.vidal.burgos@gmail.com>
2021-05-09 18:19:52 -0400

    Crear Header.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-const App = () => {
+import {PantallaIngresoCliente} from './views/PantallaIngresoCliente.js'
+
+export const App = () => {
   return (
     <div>
       <h1>HOLIWIS</h1>
+      <PantallaIngresoCliente />
     </div>
   );
 }
 
-export default App;
