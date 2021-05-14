24e63e5e8bc64f730d7ee5aa79584c386eb0e2b9
gonzalo <goyarzunpanes@gmail.com>
2021-05-14 01:12:41 -0400

    se realizo navbar y cambios minimos en el header

---
+++ src/components/Navbar.js
@@ -0,0 +1,37 @@
+import { Search, ShoppingCart } from "@material-ui/icons";
+
+export const Navbar = () => {
+  return (
+    <section className="navbar">
+      <div className="container-list">
+        <ul className="list">
+          <a href="/">
+            <li>Ofertas</li>
+          </a>
+          {/* Selecciona un tipo de producto: */}
+          <a href="./mujer.html">
+            <li>Mujer</li>
+          </a>
+          <a href="./hombre.html">
+            <li>Hombre</li>
+          </a>
+          <a href="./niños.html">
+            <li>Niños</li>
+          </a>
+          <a href="./unisex.html">
+            <li>Unisex</li>
+          </a>
+        </ul>
+      </div>
+      <div className="right-nav">
+        <input type="text" />
+        <a className="container-search" href="./pantallaBusqueda.html">
+          <Search />
+        </a>
+        <a href="./carritoDeCompras.html">
+          <ShoppingCart />
+        </a>
+      </div>
+    </section>
+  );
+};
--- src/views/PantallaIngresoCliente.js
+++ src/views/PantallaIngresoCliente.js
@@ -1,64 +1,71 @@
-import { Card } from "../components/Card.js"
-import { Header } from "../components/Header.js"
+import { Card } from "../components/Card.js";
+import { Header } from "../components/Header.js";
+import { Navbar } from "../components/Navbar.js";
 
-const iconcitoLink = 'https://res.cloudinary.com/initial-cloud/image/upload/v1620870788/carrito_zs5b0x.png'
+const iconcitoLink =
+  "https://res.cloudinary.com/initial-cloud/image/upload/v1620870788/carrito_zs5b0x.png";
 
 const productos = [
   {
     id: 1,
-    img: 'https://res.cloudinary.com/initial-cloud/image/upload/v1620870405/shirt_kbryht.png',
-    nombre: 'polera',
-    talla: 'L',
+    img: "https://res.cloudinary.com/initial-cloud/image/upload/v1620870405/shirt_kbryht.png",
+    nombre: "polera",
+    talla: "L",
     estado: "disponible",
     color: "gris",
     precio: "9990",
     tipo: "Hombre",
-    categoria: "hombres"
+    categoria: "hombres",
   },
   {
     id: 2,
-    img: 'https://res.cloudinary.com/initial-cloud/image/upload/v1620870405/shirt_kbryht.png',
-    nombre: 'pantalon',
-    talla: 'S',
+    img: "https://res.cloudinary.com/initial-cloud/image/upload/v1620870405/shirt_kbryht.png",
+    nombre: "pantalon",
+    talla: "S",
     estado: "disponible",
     color: "negro",
     precio: "5990",
     tipo: "Hombre",
-    categoria: "hombres"
+    categoria: "hombres",
   },
   {
     id: 3,
-    img: 'https://res.cloudinary.com/initial-cloud/image/upload/v1620870405/shirt_kbryht.png',
-    nombre: 'polera',
-    talla: 'L',
+    img: "https://res.cloudinary.com/initial-cloud/image/upload/v1620870405/shirt_kbryht.png",
+    nombre: "polera",
+    talla: "L",
     estado: "disponible",
     color: "gris",
     precio: "9990",
     tipo: "Hombre",
-    categoria: "hombres"
+    categoria: "hombres",
   },
   {
     id: 4,
-    img: 'https://res.cloudinary.com/initial-cloud/image/upload/v1620870405/shirt_kbryht.png',
-    nombre: 'polera',
-    talla: 'L',
+    img: "https://res.cloudinary.com/initial-cloud/image/upload/v1620870405/shirt_kbryht.png",
+    nombre: "polera",
+    talla: "L",
     estado: "disponible",
     color: "gris",
     precio: "9990",
     tipo: "Hombre",
-    categoria: "hombres"
+    categoria: "hombres",
   },
-]
+];
 
 export const PantallaIngresoCliente = () => {
   return (
     <div>
       <Header />
-      {
-        productos.map((producto) => {
-          return <Card key={producto.id} imagen={ producto.img } iconito={ iconcitoLink } />
-        })
-      }
+      <Navbar />
+      {productos.map((producto) => {
+        return (
+          <Card
+            key={producto.id}
+            imagen={producto.img}
+            iconito={iconcitoLink}
+          />
+        );
+      })}
     </div>
-  )
-}
+  );
+};
