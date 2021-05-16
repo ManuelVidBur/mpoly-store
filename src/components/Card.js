95433168c3e86236d32ff859cd1c959187a238ce
gonzalo <goyarzunpanes@gmail.com>
2021-05-15 20:12:39 -0400

    se le agregaron default props al componente Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,15 @@
-import { Button } from './Button.js'
+import { Button } from "./Button.js";
 
-export const Card = (props) => {
-
+export const Card = ({
+  imagen = "https://res.cloudinary.com/initial-cloud/image/upload/v1620870405/shirt_kbryht.png",
+  iconito = "https://res.cloudinary.com/initial-cloud/image/upload/v1620870788/carrito_zs5b0x.png",
+}) => {
   return (
     <div className="card">
-      <img className="br" src={ props.imagen } alt="producto" />
+      <img className="br" src={imagen} alt="producto" />
       <div className="footer-card">
-        <Button iconitoButton={props.iconito} />
+        <Button iconitoButton={iconito} />
       </div>
     </div>
-  )
-}
+  );
+};
