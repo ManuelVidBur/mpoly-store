2c3af548f82fb639fc28d50fb7f31b66db4098f2
gonzalo <goyarzunpanes@gmail.com>
2021-05-19 00:44:23 -0400

    a

--- src/views/PantallaMetodoPago.js
+++ src/views/PantallaMetodoPago.js
@@ -1,6 +1,5 @@
 import { BoxPago } from "../components/BoxPago";
 import { Header } from "../components/Header";
-import { Soporte } from "../components/Soporte";
 
 export const PantallaMetodoPago = () => {
   return (
@@ -8,15 +7,13 @@ export const PantallaMetodoPago = () => {
       <Header />
 
       <section>
-        <h2 className="metodo-pago-text1">Seleccionar Método de Pago</h2>
+        <h2 className="subtitulo-retiro-local">Seleccionar Método de Pago</h2>
       </section>
 
       <section className="d-flex">
         <BoxPago titulo="Transferencia Electrónica" imagenCentral="https://res.cloudinary.com/repro/image/upload/v1621190800/logo_efectivo_ms6zjw.jpg" />
         <BoxPago titulo="Pagar por Stripe" imagenCentral="https://res.cloudinary.com/repro/image/upload/v1621217353/stripe-payment-logo_tpz97u.png" />
       </section>
-
-      <Soporte />
     </>
   );
 };
